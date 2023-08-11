59904caaf7336af04aaf15ca02f098f52007e8e5
Ahmed <ahmedyassingz67@gmail.com>
2023-08-10 17:46:53 -0700

    first

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	printf("Size of a char: %li byte(s)\n", sizeof(char));
-	printf("Size of a int: %li byte(s)\n", sizeof(int));
+	printf("Size of an int: %li byte(s)\n", sizeof(int));
 	printf("Size of long int: %li byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %li byte(s)\n", sizeof(long long int));
 	printf("Size of float: %li byte(s)\n", sizeof(float));
