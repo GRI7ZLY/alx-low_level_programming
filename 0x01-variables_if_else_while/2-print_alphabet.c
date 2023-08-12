c7126834fe7389ada397d3fa80366fe41f50d13b
Ahmed <ahmedyassingz67@gmail.com>
2023-08-11 17:18:29 -0700

    Third one

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ *
+ * description: This is a putchar code that should print the alphabet
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
