adcc1ddc700c4714ad096b19b285cfdb7d805b4a
Ahmed <ahmedyassingz67@gmail.com>
2023-08-11 18:11:14 -0700

    number 8 done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ *
+ * description: siht eno dluohs tuptuo eht tebahpla
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+		return (0);
+}
