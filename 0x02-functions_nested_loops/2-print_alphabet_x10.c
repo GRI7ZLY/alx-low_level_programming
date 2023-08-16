945b53f02eb94bab1577099beaba609f04642104
Ahmed <ahmedyassingz67@gmail.com>
2023-08-15 18:18:58 -0700

    aa

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int y;
+
+	y = 0;
+	
+		while (y < 10)
+		{
+			x = 'a';
+				while (x < 'z')
+			{
+					_putchar(x);
+						x++;
+			}
+		_putchar('\n');
+			y++;
+		}
+}
+
+
