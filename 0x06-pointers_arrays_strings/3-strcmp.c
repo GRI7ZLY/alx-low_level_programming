a118b33d2a3146e32540668ba0e8b1e32369a4b1
Ahmed <ahmedyassingz67@gmail.com>
2023-08-24 12:40:30 -0700

    working

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - string to compare
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: string value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+
+	while (s1[x] == s2[x] && s1[x] != '\0')
+		x++;
+	return (s1[x] - s2[x]);
+}
